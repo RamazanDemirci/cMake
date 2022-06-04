ab9646329ee792afbb580c1d47b5c594c76c9c38
RamazanDemirci <radem18@gmail.com>
2022-06-04 20:43:09 +0300

    add submodules pull down

--- Readme.md
+++ Readme.md
@@ -1,4 +1,10 @@
 
+# configure and build with cmake using gui
+
+./confugure.sh
+./build/sh
+./run.sh
+
 
 mkdir -p out/build
 
@@ -9,3 +15,9 @@ cmake -S . -B out/build/
 
 Lesson 1 : https://www.youtube.com/watch?v=nlKcXPUJGwA&list=PLalVdRk2RC6o5GHu618ARWh0VO0bFlif4
 Lesson 2 : https://www.youtube.com/watch?v=kEGQKzhciKc&list=PLalVdRk2RC6o5GHu618ARWh0VO0bFlif4&index=2
+Lesson 3 : https://www.youtube.com/watch?v=ED-WUk440qc&list=PLalVdRk2RC6o5GHu618ARWh0VO0bFlif4&index=3
+
+
+# git submodules
+
+git submodule add https://github.com/glfw/glfw.git external/glfw
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Adder/adder.h"
+#include <GLFW/glfw3.h>
 
 int main(){
   std::cout << "Helllo, Ramazan";
@@ -7,6 +8,33 @@ int main(){
   float res = adder(13.4, 45.32);
 
   std::cout << "\nresult_2 : " << res;
+
+  GLFWwindow *window;
+
+  if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
 
   return 0;
 }
